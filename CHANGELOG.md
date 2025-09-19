9ab53acb3016f9981e7046ba0e903c0fc8a341c2
minoru <minoru@example.com>
2025-09-19 23:15:13 +0900

    chore(lint): fix ESLint config for TS project service

---
+++ CHANGELOG.md
@@ -0,0 +1,23 @@
+# Changelog
+All notable changes to this project will be documented in this file.
+
+## [Unreleased]
+### Added
+-
+### Changed
+-
+### Fixed
+-
+### Deprecated
+-
+### Removed
+-
+### Security
+-
+### Breaking Changes
+-
+
+## [1.0.0] - 2025-09-19
+### Added
+- Initial managed release of QRCodeGeneratorGH PCF
+- Output property qrDataUrl (PNG Data URL) for Power Automate integration
--- PCFProject/eslint.config.mjs
+++ PCFProject/eslint.config.mjs
@@ -3,11 +3,25 @@ import microsoftPowerApps from "@microsoft/eslint-plugin-power-apps";
 import pluginPromise from "eslint-plugin-promise";
 import globals from "globals";
 import typescriptEslint from "typescript-eslint";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {
-    ignores: ["**/generated/"],
+    ignores: [
+      "**/generated/**",
+      "**/node_modules/**",
+      "**/out/**",
+      "**/dist/**",
+      "Solutions/bin/**",
+      "Solutions/obj/**",
+      // config files
+      "eslint.config.mjs",
+    ],
   },
   eslintjs.configs.recommended,
   ...typescriptEslint.configs.recommended,
@@ -25,10 +39,12 @@ export default [
         ComponentFramework: true,
       },
       parserOptions: {
-        ecmaVersion: 2020,
+        ecmaVersion: 2022,
         sourceType: "module",
         projectService: true,
-        tsconfigRootDir: import.meta.dirname,
+        tsconfigRootDir: __dirname,
+        project: ["./tsconfig.json"],
+        allowDefaultProject: true,
       },
     },
 
